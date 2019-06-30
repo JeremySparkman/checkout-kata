71e63f30acbaea8e43bd402ce34686013d794dcc
jeremy <jeremyjames411@gmail.com>
2019-06-29 22:44:59 -0400

    Add Cart class

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 module.exports = {
   StoreItem : class {
     
+  },
+  Cart : class {
+
   }
 }
--- app.test.js
+++ app.test.js
@@ -4,4 +4,7 @@ describe('setup objects', () => {
   test('expect StoreItem to be defined', () => {
     expect(app.StoreItem).toBeDefined();
   });
+  test('expect Cart to be defined', () => {
+    expect(app.Cart).toBeDefined();
+  });
 });
